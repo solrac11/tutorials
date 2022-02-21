f85aa5453f9d2d8b869b4cea6b08b382f4a3bc86
Anton Putra <me@antonputra.com>
2022-02-22 01:50:44 +0500

    asd

--- lessons/108/terraform/9-service-account.tf
+++ lessons/108/terraform/9-service-account.tf
@@ -11,7 +11,7 @@ resource "google_project_iam_member" "service-a" {
 }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account_iam
-resource "google_service_account_iam_member" "gce-default-account-iam" {
+resource "google_service_account_iam_member" "service-a" {
   service_account_id = google_service_account.service-a.id
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:devops-v4.svc.id.goog[staging/service-a]"

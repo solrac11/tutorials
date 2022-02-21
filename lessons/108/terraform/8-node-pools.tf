594030aaa203974f036bb6ffa06e3347f9163ee6
Anton Putra <me@antonputra.com>
2022-02-21 06:16:44 +0500

    tmp

--- lessons/108/terraform/8-node-pools.tf
+++ lessons/108/terraform/8-node-pools.tf
@@ -23,9 +23,41 @@ resource "google_container_node_pool" "general" {
       role = "general"
     }
 
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+}
+
+resource "google_container_node_pool" "spot" {
+  name       = "spot"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 10
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+
+    labels = {
+      role = "spot"
+    }
+
     taint {
-      key    = "team"
-      value  = "devops"
+      key    = "type"
+      value  = "spot"
       effect = "NO_SCHEDULE"
     }
 

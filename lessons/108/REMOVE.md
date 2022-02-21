f7ef47b076a70cba1ab712fffce69104a15d6fd0
Anton Putra <me@antonputra.com>
2022-02-21 05:40:55 +0500

    qwe

--- lessons/108/REMOVE.md
+++ lessons/108/REMOVE.md
@@ -2,3 +2,15 @@
 gcloud auth application-default login
 
 gcloud alpha storage ls --project devops-v4 --all-versions gs://antonputra-tf-state-staging
+
+
+
+<!-- provider -->
+you must have gcloud installed
+
+https://www.terraform.io/language/settings/backends/gcs
+Warning! It is highly recommended that you enable Object Versioning on the GCS bucket to allow for state recovery in the case of accidental deletions and human error.
+
+gcloud config configurations list
+cat ~/.config/gcloud/configurations/config_default
+gcloud init
--- lessons/108/terraform/1-provider.tf
+++ lessons/108/terraform/1-provider.tf
@@ -1,18 +1,10 @@
-# you must have gcloud installed
-
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
 provider "google" {
   project = "devops-v4"
   region  = "us-central1"
 }
 
-
 # https://www.terraform.io/language/settings/backends/gcs
-# Warning! It is highly recommended that you enable Object Versioning on the GCS bucket to allow for state recovery in the case of accidental deletions and human error.
-
-# gcloud config configurations list
-# cat ~/.config/gcloud/configurations/config_default
-# gcloud init
-
 terraform {
   backend "gcs" {
     bucket = "antonputra-tf-state-staging"
